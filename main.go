206e838a8b579da1c82c634d7c2c02c9a232a2e1
tdaira <asada09403@gmail.com>
2018-08-10 22:13:39 +0900

    Create sub directory.

--- main.go
+++ main.go
@@ -4,24 +4,18 @@ import (
 	"github.com/tdaira/gocrawl"
 	"net/http"
 	"time"
-	"regexp"
 	"github.com/PuerkitoBio/goquery"
 	"crypto/sha1"
 	"os"
 	"bytes"
 	"encoding/base64"
+	"strings"
 )
 
-// Only enqueue the yahoo news domain.
-var rxOk = regexp.MustCompile(`http://news\.yahoo\.co\.jp.*`)
-
-// Create the Extender implementation, based on the gocrawl-provided DefaultExtender,
-// because we don't want/need to override all methods.
 type ExampleExtender struct {
-	gocrawl.DefaultExtender // Will use the default implementation of all but Visit and Filter
+	gocrawl.DefaultExtender
 }
 
-// Override Visit for our need.
 func (x *ExampleExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
 	bufBody := new(bytes.Buffer)
 	bufBody.ReadFrom(res.Body)
@@ -32,7 +26,11 @@ func (x *ExampleExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc
 	sha := sha1.New()
 	sha.Write([]byte(url.String()))
 	shaStr := base64.URLEncoding.EncodeToString(sha.Sum(nil))
-	file, err := os.Create("./data/" + shaStr)
+	outDir := "./data/" + strings.Replace(url.Host, ".", "_", -1)
+	if _, err := os.Stat(outDir); os.IsNotExist(err) {
+		os.Mkdir(outDir, os.ModePerm)
+	}
+	file, err := os.Create(outDir + "/" + shaStr)
 	if err != nil {
 		panic(err)
 	}
@@ -42,28 +40,17 @@ func (x *ExampleExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc
 	return nil, true
 }
 
-// Override Filter for our need.
-func (x *ExampleExtender) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
-	return !isVisited && rxOk.MatchString(ctx.NormalizedURL().String())
-}
-
 func main() {
-	// Set custom options
 	opts := gocrawl.NewOptions(new(ExampleExtender))
 
-	// should always set your robot name so that it looks for the most
-	// specific rules possible in robots.txt.
 	opts.RobotUserAgent = "Example"
-	// and reflect that in the user-agent string used to make requests,
-	// ideally with a link so site owners can contact you if there's an issue
 	opts.UserAgent = "Mozilla/5.0 (compatible; Example/1.0; +http://example.com)"
 
 	opts.CrawlDelay = 1 * time.Second
-	opts.LogFlags = gocrawl.LogAll
+	opts.LogFlags = gocrawl.LogEnqueued
 
 	opts.MaxVisits = 20
 
-	// Create crawler and start at root of duckduckgo
 	c := gocrawl.NewCrawlerWithOptions(opts)
-	c.Run("https://news.yahoo.co.jp/")
+	c.Run([]string{"https://news.yahoo.co.jp/", "https://headlines.yahoo.co.jp/"})
 }
